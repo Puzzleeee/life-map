ddc19e46bef9b99c1fb085406b725b8fdc9b6843
Lau Siaw Sam <lausiawsam98@gmail.com>
2020-05-17 15:59:53 +0800

    Added dependencies.

---
+++ life-map/src/App.js
@@ -0,0 +1,13 @@
+import react from "react";
+import styled from "styled-components";
+
+function App() {
+  return <CustomHeader>LifeMap</CustomHeader>;
+}
+
+export default App;
+
+const CustomHeader = styled.h1`
+  display: flex;
+  color: black;
+`
