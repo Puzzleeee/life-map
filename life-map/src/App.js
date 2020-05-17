c55998873e54bf87ac7cb2c064e58bf26749443e
Lau Siaw Sam <lausiawsam98@gmail.com>
2020-05-17 17:05:09 +0800

    Added api call to get fb id

--- life-map/src/App.js
+++ life-map/src/App.js
@@ -1,9 +1,44 @@
-import react from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import styled from "styled-components";
 
-function App() {
-  return <CustomHeader>LifeMap</CustomHeader>;
-}
+const App = () => {
+  let facebookID = undefined;
+
+  useEffect(() => {
+    (async () => {
+      facebookID = axios.get();
+    })();
+  });
+
+  const handleClick = () => {
+    console.log("logging in");
+  };
+
+  const handleResponse = (response) => {
+    console.log("got response");
+    console.log(response);
+  };
+
+  return (
+    <Layout>
+      <TitleContainer>
+        <Title>LifeMap</Title>
+      </TitleContainer>
+
+      <BodyContainer>
+        <p>Welcome to LifeMap.</p>
+        <FacebookLogin
+          appId={facebookID}
+          autoload={true}
+          fields="name,email"
+          onClick={handleClick}
+          callback={handleResponse}
+        />
+      </BodyContainer>
+    </Layout>
+  );
+};
 
 export default App;
 
