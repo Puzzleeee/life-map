c8b2c1e96092ee388477b2bd4df5d88dc6fd3bfe
Puzzleeee <48142301+Puzzleeee@users.noreply.github.com>
2020-05-27 13:01:58 +0800

    Correct require statements after file name change

--- life-map/route_includes/homepage_routes.js
+++ life-map/route_includes/homepage_routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/homepage', require('../routes/homepage/markers.js'));
+router.use('/homepage', require('../routes/homepage/home.js'));
 
 module.exports = router;
