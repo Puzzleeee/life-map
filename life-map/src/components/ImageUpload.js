4f1545dc9a662bf4dfb8c254e50e06d374985fac
Lau Siaw Sam <lausiawsam98@gmail.com>
2020-06-13 14:37:22 +0800

    ImageUpload: add basic styling

--- life-map/src/components/ImageUpload.js
+++ life-map/src/components/ImageUpload.js
@@ -15,14 +15,42 @@ const ImageUpload = () => {
   };
 
   return (
-    <div>
+    <Container>
       <input type="file" onChange={handleSelectImage} multiple />
-      {image.map((url) => (
-        <img src={url} key={url} />
-      ))}
-      {/* <img src={image} alt="selected" /> */}
-    </div>
+      <ThumbnailContainer>
+        {image.map((url) => (
+          <Thumbnail src={url} key={url} />
+        ))}
+      </ThumbnailContainer>
+    </Container>
   );
 };
 
 export default ImageUpload;
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ThumbnailContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  flex-wrap: wrap;
+
+  margin-top: 12px;
+  background-color: rgba(85, 85, 85, 0.1);
+  border-radius: 5px;
+`;
+
+const Thumbnail = styled.img`
+  width: 150px;
+  height: auto;
+  object-fit: contain;
+
+  border: 1px solid rgba(45, 45, 45, 0.5);
+  border-radius: 5px;
+  margin: 16px;
+`;
