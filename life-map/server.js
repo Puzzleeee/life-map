bbd9b41240660553e88769bc9f4b230aa0f4964f
Puzzleeee <48142301+Puzzleeee@users.noreply.github.com>
2020-07-16 18:09:18 +0800

    Fix path for static files

--- life-map/server.js
+++ life-map/server.js
@@ -13,14 +13,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('/build'));
+  app.use(express.static(path.join(__dirname, 'build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   })
 }
-
-dotenv.config({ path: "./config/config.env" });
 
 // Helmet
 app.use(helmet());
