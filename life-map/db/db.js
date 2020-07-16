b18f145f61e5f9a0b80dc2fff811c35e0df08847
Puzzleeee <48142301+Puzzleeee@users.noreply.github.com>
2020-07-16 16:47:30 +0800

    Connect to ClearDB

--- life-map/db/db.js
+++ life-map/db/db.js
@@ -1,13 +1,15 @@
 const mysql = require("mysql2");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config/config.env" });
 
 var pool = mysql.createPool({
   multipleStatements: true,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "orbital_development",
+  host: `${process.env.DB_HOST}`,
+  user: `${process.env.DB_USER}`,
+  password: `${process.env.DB_PASSWORD}`,
+  database: `${process.env.DB_DATABASE}`,
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 9,
   queueLimit: 0,
 });
 
