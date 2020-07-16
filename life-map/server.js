d6f14a2ed66e4428e141e695ecc21149ae5345fe
Puzzleeee <48142301+Puzzleeee@users.noreply.github.com>
2020-07-16 17:05:39 +0800

    Set up for pushing to heroku

--- life-map/server.js
+++ life-map/server.js
@@ -8,9 +8,18 @@ const initializePassport = require("./passport-config.js");
 const busboy = require("connect-busboy");
 const busboyBodyParser = require("busboy-body-parser");
 const helmet = require("helmet");
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  })
+}
+
 dotenv.config({ path: "./config/config.env" });
 
 // Helmet
